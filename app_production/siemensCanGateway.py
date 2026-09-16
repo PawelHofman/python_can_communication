f488fcd108e508ff9c70c8121ebee1e138973885
hofman <hofman@L7P-0117>
2026-09-16 15:43:37 +0200

    dodanie 3 zmiennych oraz zmiana cyklu komuniakcji

--- app_production/siemensCanGateway.py
+++ app_production/siemensCanGateway.py
@@ -1,5 +1,8 @@
 # ==========================================================
-# WERSJA 0006
+# WERSJA 0006_1_2_3
+# _1 dodano zmienna IN_MODE
+# _2 dodano zmienna RECEIVED_3A0
+# _2 dodano zmienna MODIFIED_1_4_3A0_SENT
 # ==========================================================
 
 # ==========================================================
@@ -28,6 +31,18 @@ NO_PILOT_ACTION = 0
 
 PILOT_ALIVE = 0
 LAST_PILOT_MSG_TIME = 0
+IN_MODE = 99
+# JESLI 99 TO NIE MA TRYBU 
+# 1 = NO PILOT
+# 2 = TRANSPARETNY 
+# 3 = TRANSPARETNY + OGRANICZENIE PREDKOSCI 
+# IN_MODE = 10 + byte1 
+    # 11 - TRYB 1 
+    # 12 - TRYB 2 
+    # 13 - TRYB 3 
+    # 14 - TRYB 4 
+RECEIVED_3A0 = 0 # LICZNIK ODEBRANYCH RAMEK 3A0 | 8 BYTE
+MODIFIED_1_4_3A0_SENT = 0 # licznik zmodyfikowanych ramek w modach 1-4
 
 import can
 import threading
@@ -58,6 +73,10 @@ def plc_thread():
     global NO_PILOT_ACTION
     global PILOT_ALIVE
     global LAST_PILOT_MSG_TIME
+    global IN_MODE
+    global RECEIVED_3A0
+    global MODIFIED_1_4_3A0_SENT
+
 
     client = snap7.client.Client()
 
@@ -134,10 +153,13 @@ def plc_thread():
             # ==================================================
             life = int(time.time()) % 2
 
-            send_buf = bytearray(2)
+            send_buf = bytearray(5)
 
             send_buf[0] = life
             send_buf[1] = PILOT_ALIVE
+            send_buf[2] = IN_MODE
+            send_buf[3] = RECEIVED_3A0
+            send_buf[4] = MODIFIED_1_4_3A0_SENT
 
             client.db_write(
                 DB_RECV,
@@ -170,8 +192,15 @@ threading.Thread(
     daemon=True
 ).start()
 
+
+# ==========================================================
+# ==========================================================
+# ==========================================================
+# ==========================================================
+# ==========================================================
 # ==========================================================
 # CAN
+# ==========================================================
 # ==========================================================
 print(f"Pilot on  : {PILOT_CAN}")
 print(f"System on : {SYSTEM_CAN}")
@@ -201,6 +230,11 @@ class PilotToSystem(can.Listener):
 
         LAST_PILOT_MSG_TIME = time.time()
 
+        global IN_MODE
+        global RECEIVED_3A0
+        global MODIFIED_1_4_3A0_SENT
+
+
         try:
 
             # ==================================================
@@ -210,6 +244,20 @@ class PilotToSystem(can.Listener):
             speed = PLC_SPEED
             speed_limit = SPEED_LIMIT_ACTIVE
             no_pilot = NO_PILOT_ACTION
+
+            # IN_MODE = 99
+
+            # ==================================================
+            # LICZENIE RAMEK arbitration_id == 0x3A0 & dlc == 8
+            # ==================================================
+            if (
+                msg.arbitration_id == 0x3A0
+                and msg.dlc == 8
+            ):
+                RECEIVED_3A0 = RECEIVED_3A0 + 1
+
+                if (RECEIVED_3A0 > 250):
+                    RECEIVED_3A0 = 0
 
             # ==================================================
             # NO PILOT ACTION
@@ -239,6 +287,8 @@ class PilotToSystem(can.Listener):
 
                 # print("BLOCKED:", data.hex())
 
+                IN_MODE = 1
+
                 msg = can.Message(
                     arbitration_id=msg.arbitration_id,
                     data=data,
@@ -256,6 +306,8 @@ class PilotToSystem(can.Listener):
                 mode == 0
                 and no_pilot == 0
             ):
+
+                IN_MODE = 2
 
                 if (
                     speed_limit == 1
@@ -276,6 +328,8 @@ class PilotToSystem(can.Listener):
                                 data[i],
                                 limit
                             )
+
+                    IN_MODE = 3
 
                     msg = can.Message(
                         arbitration_id=msg.arbitration_id,
@@ -303,12 +357,23 @@ class PilotToSystem(can.Listener):
                 data[1] = byte1
                 data[indexX] = speed
 
+                IN_MODE = 10 + mode
+
                 msg = can.Message(
                     arbitration_id=msg.arbitration_id,
                     data=data,
                     dlc=msg.dlc,
                     is_extended_id=msg.is_extended_id
                 )
+
+                system_bus.send(msg)
+
+                MODIFIED_1_4_3A0_SENT = MODIFIED_1_4_3A0_SENT + 1
+                
+                if (MODIFIED_1_4_3A0_SENT > 250):
+                    MODIFIED_1_4_3A0_SENT = 0
+
+                return
 
             # ==================================================
             # SEND
